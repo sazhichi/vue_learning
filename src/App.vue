2e99d7a4a0d33e04520b7fd89e791ab8a56eb104
sazhichi <13276141912@163.com>
2024-04-17 12:51:39 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- HTML-->
+  <div>
+    <ul>
+      <li v-for="(item,index) in message_list" :key="index">
+        {{item}}
+      </li>
+    </ul>
+    <input v-model="input_message"/>
+    <button @click="handleClick()">点击我</button>
+  </div>
+</template>
+
+
+
+<script setup>
+import {ref} from "vue";
+
+const message_list = ref([]);
+
+const input_message = ref(null);
+
+const handleClick = () =>{
+  message_list.value.push(input_message.value);
+};
+</script>
+
+
+<style scoped>
+</style>
